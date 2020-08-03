aeba3fe282bd12306db89c44ba083bca2357c623
eiichikrym <eiichikrym@gmail.com>
2020-08-03 16:46:33 +0900

    FactoryBotの記述を編集
    fakerとgimeiという2種のgemを使用

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    gimei = Gimei.name
+    last_name             {gimei.last.kanji}
+    first_name            {gimei.first.kanji}
+    last_name_kana        {gimei.last.katakana}
+    first_name_kana       {gimei.first.katakana}
+  end
+end

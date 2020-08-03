a087ab79ec397d5f4d4a7d6047ecf7e66ec39ed7
eiichikrym <eiichikrym@gmail.com>
2020-08-03 12:45:36 +0900

    userモデル編集（バリデーション記述）
    新規登録の際のバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,43 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # , :validatable deviseの初期設定を外す
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: { minimum: 6 }, format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./ }
+    validates :password_confirmation, confirmation: true
+    validates :birthday
+
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
+
 end
+
+# 実装の条件
+## ユーザー情報
+# - ニックネームが必須であること
+# - メールアドレスが必須であること
+# - メールアドレスが一意性であること
+# - メールアドレスは@を含む必要があること
+# - パスワードが必須であること
+# - パスワードは6文字以上であること 
+# - パスワードは半角英数字混合であること
+# - パスワードは確認用を含めて2回入力すること
+##本人情報確認
+# - ユーザー本名が、名字と名前がそれぞれ必須であること
+# - ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること
+# - ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること
+# - ユーザー本名のフリガナは全角（カタカナ）で入力させること
+# - 生年月日が必須であること

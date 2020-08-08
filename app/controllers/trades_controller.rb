89d698679fbabdff0834bc37bdc07d8132be778e
eiichikrym <eiichikrym@gmail.com>
2020-08-08 20:49:46 +0900

    tradesコントローラー へ定義したtrading_informationを記述

---
+++ app/controllers/trades_controller.rb
@@ -0,0 +1,37 @@
+class TradesController < ApplicationController
+  before_action :check_trade
+
+  def index
+    @item = Item.find(params[:item_id])
+    if user_signed_in? && (current_user.id == @item.user_id) 
+      redirect_to root_path
+    elsif user_signed_in?
+      @trade = TradingInformation.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @trade = TradingInformation.new(trade_params)
+    if @trade.valid?
+      @trade.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render action: :index
+    end
+  end
+
+private
+  def check_trade
+    @item = Item.find(params[:item_id])
+      if @item.trade != nil
+        redirect_to root_path
+      end
+  end
+
+  def trade_params
+    params.require(:trading_information).permit(:postcode, :state_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
